8ecccc16cd37a4fb5201e50d52915d75d2c829d7
gulvekartik45 <kartikgulve2002@gmail.com>
2025-07-30 13:05:15 +0530

    Added GitHub to learning message

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
 public class First{
     public static void main (String args[]){
-        System.out.println( "I am currently learning Git");
+        System.out.println( "I am currently learning Git and GitHub");
     }
 }
