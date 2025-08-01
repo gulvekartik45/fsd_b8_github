2fd0f5f91852e11a7ffbbf219c838142efb01afe
gulvekartik45 <kartikgulve2002@gmail.com>
2025-08-01 17:52:19 +0530

    my first commit on newBranch

---
+++ GitDemo/second.java
@@ -0,0 +1,6 @@
+public class second{
+    public static void main (String args[]){
+        System.out.println( "I am currently learning Git and GitHub");
+        System.out.println("hello kartik");
+    }
+}
