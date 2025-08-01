65ae885354358208930507d3eace69fddd408966
gulvekartik45 <kartikgulve2002@gmail.com>
2025-08-01 18:16:15 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First{
     public static void main (String args[]){
         System.out.println( "I am currently learning Git and GitHub");
+        System.out.println( " i am kartik gulve");
     }
 }
